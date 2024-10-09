d19f232a3faac216649b41ac5b5020d0dc4ea64e
danclarke95 <dan.clarke1995@gmail.com>
2024-10-09 14:07:30 +0100

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +12,39 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
+// Configure Kestrel to use the backend.pfx certificate
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ConfigureHttpsDefaults(listenOptions =>
+    {
+        // Path to the PFX file
+        var certPath = "/home/danclarke/Documents/CProjects/DatingAppNew/client/ssl/backend.pfx";
+        
+        // If you set an export password when generating the PFX, use it here
+        var certPassword = "ron!XD7557"; // Replace with your password if set
+
+        // Load the certificate
+        listenOptions.ServerCertificate = new X509Certificate2(certPath, certPassword);
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Enable HTTPS redirection
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// Configure CORS to allow requests from Angular frontend
+app.UseCors(policy => 
+    policy.WithOrigins("https://localhost:4200")
+          .AllowAnyHeader()
+          .AllowAnyMethod()
+);
+
+//app.UseAuthorization();
 
 app.MapControllers();
 
